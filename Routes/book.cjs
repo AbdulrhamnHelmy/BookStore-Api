05c76e970040220ddbccf5ea9e0782cfba59883b
Abdulrahman Helmy <118119414+AbdulrhamnHelmy@users.noreply.github.com>
2024-11-24 13:42:45 +0200

    Update book.cjs

--- Routes/book.cjs
+++ Routes/book.cjs
@@ -3,10 +3,10 @@ const bookController = require("../Controllers/bookController.cjs");
 const authMiddleWare = require("../middlewares/auth.cjs");
 const router = Router.Router();
 
-router.post("/api/books", authMiddleWare, bookController.addNewBook);
-router.get("/api/books", authMiddleWare, bookController.getAllBooks);
-router.get("/api/books/:id", authMiddleWare, bookController.getOneBook);
-router.put("/api/books/:id", authMiddleWare, bookController.updateBook);
-router.delete("/api/books/:id", authMiddleWare, bookController.deleteBook);
+router.post("/api/books", authMiddleWare, bookController.addNewBook); // Only Admin
+router.get("/api/books", bookController.getAllBooks);
+router.get("/api/books/:id", bookController.getOneBook);
+router.put("/api/books/:id", authMiddleWare, bookController.updateBook); // Only Admin
+router.delete("/api/books/:id", authMiddleWare, bookController.deleteBook); // Only Admin
 
 module.exports = router;
